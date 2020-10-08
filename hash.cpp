b56189eb4cfa35292dfb3eb7c88094b7628f9774
Tadas Mockevičius <tadasmoc@gmail.com>
2020-10-09 00:33:37 +0300

    first prototype

--- hash.cpp
+++ hash.cpp
@@ -46,7 +46,7 @@ void Hash::toBinary(std::string input)
 		bin += std::bitset<8>(input[i]).to_string();
 	}
 
-	std::cout << "converted to bin: " << bin << std::endl;
+	//std::cout << "converted to bin: " << bin << std::endl;
 
 	padding(bin);
 }
@@ -59,22 +59,97 @@ void Hash::padding(std::string bin)
 			bin += "0";
 		}
 	}
-	std::cout << "padded: " << bin << std::endl;
+	//std::cout << "padded: " << bin << std::endl;
 
 	compression(bin);
 }
 
 void Hash::compression(std::string bin)
 {
-	while (bin.size() != 0) {
-		std::string block = bin.substr(bin.size() - 512, bin.size());
-		for (int i = 0; i < 512; i++) bin.pop_back(); // potentialy slow.
+	//std::vector<std::string> block512;
+	std::deque<std::string> block32; // 32bitai x 16. viso - 512.
+	std::vector<std::string> hashBin; // 32bitai x 8. viso - 256.
 
-		std::cout << "size: " << bin.size() << std::endl;
-		std::cout << "block: " << block << std::endl;
+
+	// Viskas isdeliojama po 32 bitus i block32.
+	int t = 0;
+	while (bin.size() != 0) {
+		block32.push_back(bin.substr(bin.size() - 32, bin.size()));
+		for (int i = 0; i < 32; i++) bin.pop_back(); // potentialy slow.
+
+		//std::cout << "block[" << t << "] " << block32[t] << std::endl;
+		//std::cout << "size: " << bin.size() << std::endl;
+
+		t++;
 	}
 
+	while (block32.size() != 0) {
 
-	
-	std::cout << "compression ended" << std::endl;
+		//std::cout << "po pirmo maisymo: \n";
+		for (int i = 15; i > 0; i--) {
+			block32[i - 1] = xorString(block32[i - 1], block32[i]);
+			//std::cout << "block[" << i << "] " << block32[i] << std::endl;
+		}
+
+		//std::cout << "po antro maisymo: \n";
+		for (int i = 1; i < 16; i++) {
+			std::rotate(block32[i].begin(), block32[i].begin() + i, block32[i].end());
+			//std::cout << "block[" << i << "] " << block32[i] << std::endl;
+		}
+
+		//std::cout << "po trecio maisymo: \n";
+		for (int i = 15; i > 0; i--) {
+			block32[i - 1] = xorString(block32[i - 1], block32[i]);
+			//std::cout << "block[" << i << "] " << block32[i] << std::endl;
+		}
+
+		//std::cout << "po ketvirto maisymo: \n";
+		for (int i = 0; i < 16; i += 2) {
+
+			hashBin.push_back(xorString(block32[i], block32[i + 1]));
+			//std::cout << "block[" << i << "] " << block32[i] << std::endl;
+		}
+
+
+		// TODO: butinai pakeist del avalanche efekto ir collisionu.
+		//std::cout << "po test maisymo: \n";
+		for (int i = 0; i < 8; i++) {
+			test[i] = xorString(test[i], hashBin[i]);
+			//std::cout << "block[" << i << "] " << test[i] << std::endl;
+		}
+		hashBin.clear();
+
+		for (int i = 0; i < 16; i++) block32.pop_front();
+		//std::cout << "---------------Block32 size after iteration: " << block32.size() << std::endl;
+	}
+
+	//std::cout << "compression ended" << std::endl;
+	binToHex(test);
+}
+
+std::string Hash::xorString(std::string a, std::string b)
+{
+	std::string ans;
+
+	for (int i = 0; i < a.size(); i++)
+	{
+		// If the Character matches 
+		if (a[i] == b[i])
+			ans += "0";
+		else
+			ans += "1";
+	}
+	return ans;
+}
+
+void Hash::binToHex(std::vector<std::string> hashBin)
+{
+	//std::cout << "Bin to Hex started\n\n";
+
+	for (int i = 0; i < 8; i++) {
+		//std::cout << i << ".   " << hashBin[i] << std::endl;
+		std::bitset<32> set(hashBin[i]);
+		std::cout << std::hex << set.to_ulong(); // << std::endl;
+	}
+	std::cout << std::endl;
 }
--- hash.h
+++ hash.h
@@ -1,10 +1,16 @@
 #pragma once
 
+#include <bit>
+#include <cstdint>
+#include <limits>
+
 #include <iostream>
 #include <string>
 #include <fstream>
 #include <bitset>
 #include <vector>
+#include <deque>
+
 
 class Hash
 {
@@ -12,9 +18,32 @@ private:
 	std::string input;
 	std::string hash;
 public:
+	std::string A = "01010010011000010111001101110000";
+	std::string B = "01110101011101000110100101101110";
+	std::string C = "01001100011011110111011001100101";
+	std::string D = "01110010011011110110011001110100";
+	std::string E = "01101000011001010101001001110101";
+	std::string F = "01110011011100110110100101100001";
+	std::string G = "01101110011100010111010101100101";
+	std::string H = "01100101011011100010000100111111";
+
+	std::vector<std::string> test =
+	{
+		"01010010011000010111001101110000",
+		"01110101011101000110100101101110",
+		"01001100011011110111011001100101",
+		"01110010011011110110011001110100",
+		"01101000011001010101001001110101",
+		"01110011011100110110100101100001",
+		"01101110011100010111010101100101",
+		"01100101011011100010000100111111"
+	};
+
 	void hashReadConsole();
 	void hashReadFile(std::string fileName);
 	void toBinary(std::string input);
 	void padding(std::string bin);
 	void compression(std::string bin);
+	std::string xorString(std::string a, std::string b);
+	void binToHex(std::vector<std::string> hashBin);
 };
